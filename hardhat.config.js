057eedd53de98d6b9ad10a11a443ab16a337eb2c
Nathaniel Potrepka <nspotrepka@gmail.com>
2022-12-18 13:49:35 -0800

    Hide localhost deployer

--- hardhat.config.js
+++ hardhat.config.js
@@ -39,10 +39,7 @@ module.exports = {
     },
     localhost: {
       url: 'http://127.0.0.1:8545',
-      accounts: [
-        '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
-        SIGNER_PRIVATE_KEY,
-      ],
+      accounts: [DEPLOYER_PRIVATE_KEY_LOCALHOST, SIGNER_PRIVATE_KEY],
     },
   },
   solidity: '0.8.4',
