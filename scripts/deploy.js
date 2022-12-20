5d9669025b3b2900f3f3b634c73cfe0d8e231575
Nathaniel Potrepka <nspotrepka@gmail.com>
2022-12-19 21:01:38 -0800

    Add localhost deployment code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,14 @@
 const { ethers, network } = require('hardhat')
 
-const { PAYEES, SHARES, DEFAULT_URI } = require('../secret.json')
+const inputReader = require('wait-console-input')
+
+const {
+  PAYEES,
+  SHARES,
+  DEFAULT_URI,
+  PRIVATE_SALE_ID,
+  PUBLIC_SALE_ID,
+} = require('../secret.json')
 
 const REGISTRY_ADDRESS = network.config.registryAddress
 const REGISTRY_SUBSCRIPTION_ADDRESS = network.config.registrySubscriptionAddress
@@ -8,6 +16,7 @@ const LINK_ADDRESS = network.config.linkAddress
 const WRAPPER_ADDRESS = network.config.wrapperAddress
 const SAFE_ADDRESS = network.config.safeAddress
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+const MAX_UINT32 = '4294967295'
 
 let tokenContract
 let metadataContract
@@ -67,6 +76,27 @@ async function main() {
     console.log('Metadata contract: controller updated')
     await metadataContract.transferOwnership(SAFE_ADDRESS)
     console.log('Metadata contract: owner updated')
+  } else {
+    await tokenContract.addSale(PRIVATE_SALE_ID, 3200, 2, 2, 0, MAX_UINT32)
+    console.log('Token contract: private sale added')
+
+    console.log('Press SPACE to continue')
+    inputReader.wait()
+
+    await tokenContract.removeSale(PRIVATE_SALE_ID)
+    console.log('Token contract: private sale removed')
+
+    await tokenContract.addSale(PUBLIC_SALE_ID, 3200, 2, 2, 0, MAX_UINT32)
+    console.log('Token contract: public sale added')
+
+    console.log('Press SPACE to continue')
+    inputReader.wait()
+
+    await tokenContract.removeSale(PUBLIC_SALE_ID)
+    console.log('Token contract: public sale removed')
+
+    await metadataContract.setSeed(1)
+    console.log('Metadata contract: seed updated')
   }
 }
 
